77fe7d09eef5dc375de18a696420e78a26d3925d
shelkovn <shelkovn.ar@gmail.com>
2024-12-19 10:34:18 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace products
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<producttpl> elements = new List<producttpl>();
+        List<producttpl> displayed = new List<producttpl>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            foreach(producttpl elem in stack.Items)
+            {
+                elements.Add(elem);
+            }
+
+            displayed = elements;
+            stack.Items.Clear();
+            stack.ItemsSource = displayed;
+            cmb.ItemsSource = elements.Select(elem => elem.Seller);
+        }
+
+        private void refreshdisp()
+        {
+            //stack.Items.Clear();
+            stack.ItemsSource = displayed;
+        }
+
+        private void cmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            displayed = elements.Where(n => n.Seller == e.AddedItems[0] as string).ToList();  
+            refreshdisp();
+        }
+
+        private void query_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            displayed = elements.Where(n => (n.Productname.Contains((sender as TextBox).Text) || n.Description.Contains((sender as TextBox).Text))).ToList();
+            refreshdisp();
+        }
+
+        private void price_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            displayed = elements.Where(n => n.Price)
+        }
+    }
+}
---
+++ producttpl.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace products
+{
+    /// <summary>
+    /// Логика взаимодействия для producttpl.xaml
+    /// </summary>
+    public partial class producttpl : UserControl
+    {
+
+
+        public string Imgsource
+        {
+            get { return (string)GetValue(ImgsourceProperty); }
+            set { SetValue(ImgsourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImgsourceProperty =
+            DependencyProperty.Register(nameof(Imgsource), typeof(string), typeof(producttpl), new PropertyMetadata("https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"));
+
+        public string Productname
+        {
+            get { return (string)GetValue(ProductnameProperty); }
+            set { SetValue(ProductnameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProductnameProperty =
+            DependencyProperty.Register(nameof(Productname), typeof(string), typeof(producttpl), new PropertyMetadata("Product"));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Description), typeof(string), typeof(producttpl), new PropertyMetadata("..."));
+
+        public string Seller
+        {
+            get { return (string)GetValue(SellerProperty); }
+            set { SetValue(SellerProperty, value); }
+        }
+
+        public static readonly DependencyProperty SellerProperty =
+            DependencyProperty.Register(nameof(Seller), typeof(string), typeof(producttpl), new PropertyMetadata("..."));
+
+        public string Price
+        {
+            get { return (string)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register(nameof(Price), typeof(string), typeof(producttpl), new PropertyMetadata("..."));
+
+        public string Currency
+        {
+            get { return (string)GetValue(CurrencyProperty); }
+            set { SetValue(CurrencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrencyProperty =
+            DependencyProperty.Register(nameof(Currency), typeof(string), typeof(producttpl), new PropertyMetadata("..."));
+
+        public string Instock
+        {
+            get { return (string)GetValue(InstockProperty); }
+            set { SetValue(InstockProperty, value); }
+        }
+
+        public static readonly DependencyProperty InstockProperty =
+            DependencyProperty.Register(nameof(Instock), typeof(string), typeof(producttpl), new PropertyMetadata("..."));
+
+        public producttpl()
+        {
+            InitializeComponent();
+        }
+    }
+}
